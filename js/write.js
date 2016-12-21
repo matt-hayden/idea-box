b3fcd771d5de629cea1e302e56f3fd1008ca8af2
Krystin Villeneuve <krystinv@gmail.com>
2016-12-20 17:45:31 -0700

    Add more CSS styling to cards

--- js/write.js
+++ js/write.js
@@ -8,11 +8,11 @@ $('.js-save-btn').on('click', function(){
   clearInputs();
 });
 
-function NewIdea (title, body){
+function NewIdea (title, body, quality){
   this.title = title;
   this.body = body;
   this.id = Date.now();
-  this.quality ="swill";
+  this.quality = quality || 'Swill';
 }
 
 function StoreIdea (id, idea){
@@ -27,17 +27,15 @@ function loadIdeas (id){
 function displayCard (idea){
   $('.card-section').append(
     `<section class="idea-card">
-    <li> "${idea.title}" </li>
-    <button class="delete-btn">&#xd7;</button>
     <ul class="card-box">
-    <li> "${idea.body}" </li>
-    <li> "${idea.id}" </li>
-    <li> "${idea.quality}"</li>
-    <li> "${idea.quality}"
-    <button class="up-btn">&#x2191;</button>
-    <button class="down-btn">&#x2193;</button>
-    <li>quality: swill</li>
-    </li>
+    <li class="li-title"> ${idea.title} </li>
+    <button class="delete-btn"><img src="images/delete.svg"> </img></button>
+    <li class="li-body">"${idea.body}"</li>
+    <li id="key-number" class="li-id">"${idea.id}"</li>
+    <li class="li-quality">"${idea.quality}"</li>
+    <button class="up-btn"><img src="images/upvote.svg"></img></button>
+    <button class="down-btn"><img src="images/downvote.svg"></img></button>
+    </ui>
   </section>`
   );
 }
